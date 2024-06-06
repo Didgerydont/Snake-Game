2dcf57062834f6dae02d762f5c3215268d89a580
Will Prendergast <prendygast@hotmail.com>
2024-06-06 17:28:48 +0100

    Snake working as expected.

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -6,7 +6,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
 
-
 public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
     // Snake dimensions
@@ -26,8 +25,6 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
     private ImageIcon headDown;
     private ImageIcon tail;
 
-
-
     private Timer timer;
     private int delay = 100;
 
@@ -36,27 +33,23 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
     private int score = 0;
 
     // Mouse parameters
-    private int[] mousePosX = { 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475, 500, 525, 550, 575, 600, 625, 650, 675, 700, 725, 750, 775, 800, 825, 850};
-    private int[] mousePosY = { 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475, 500, 525, 550, 575, 600, 625};
-
-
+    private int[] mousePotentialPosX = { 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475, 500, 525, 550, 575, 600, 625, 650, 675, 700, 725, 750, 775, 800, 825, 850};
+    private int[] mousePotentialPosY = { 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475, 500, 525, 550, 575, 600, 625};
 
     private ImageIcon mouseImage;
 
     private Random random = new Random();
 
-    private int xpos = random.nextInt(mousePosX.length);
-    private int ypos = random.nextInt(mousePosY.length);
+    private int mouseXPosition = random.nextInt(mousePotentialPosX.length);
+    private int mouseYPosition = random.nextInt(mousePotentialPosY.length);
+
     public GamePlay(){
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
 
         timer = new Timer(delay, this);
-        timer.restart();
-
-
-
+        timer.start();
     }
 
     @Override
@@ -111,130 +104,102 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
                 tail = new ImageIcon("src/images/tail.png");
                 tail.paintIcon(this, graphics, snakeXLength[i], snakeYLength[i]);
             }
-
-            mouseImage = new ImageIcon("src/images/mouse.png");
         }
 
+        // Draw the mouse
+        mouseImage = new ImageIcon("src/images/mouse.png");
+        mouseImage.paintIcon(this, graphics, mousePotentialPosX[mouseXPosition], mousePotentialPosY[mouseYPosition]);
 
+        // Check if the snake eats the mouse
+        if(mousePotentialPosX[mouseXPosition] == snakeXLength[0] && mousePotentialPosY[mouseYPosition] == snakeYLength[0]){
+            score += 5;
+            lengthOfSnake++;
+            mouseXPosition = random.nextInt(mousePotentialPosX.length);
+            mouseYPosition = random.nextInt(mousePotentialPosY.length);
+        }
+
+        // Draw the score
+        graphics.setColor(Color.WHITE);
+        graphics.setFont(new Font("arial", Font.PLAIN, 14));
+        graphics.drawString("Score: " + score, 780, 30);
+        graphics.drawString("Length: " + lengthOfSnake, 780, 50);
     }
-
 
     @Override
     public void actionPerformed(ActionEvent arg0) {
-        // TODO Auto-generated method stub
-        //Part4
-        timer.restart();
+        timer.start();
         // move right animation
-        if(right)
-        {
-            for(int n = lengthOfSnake-1; n>=0;n--)
-            {
+        if(right) {
+            for(int n = lengthOfSnake-1; n>=0;n--) {
                 snakeYLength[n+1] = snakeYLength[n];
             }
-            for(int n = lengthOfSnake; n>=0; n--)
-            {
-                if (n==0)
-                {
+            for(int n = lengthOfSnake; n>=0; n--) {
+                if (n==0) {
                     snakeXLength[n] = snakeXLength[n]+25;
-                }
-                else
-                {
+                } else {
                     snakeXLength[n] = snakeXLength[n-1];
                 }
-                if(snakeXLength[n] >850)
-                {
+                if(snakeXLength[n] >850) {
                     snakeXLength[n] = 25;
                 }
-
             }
             repaint();
         }
         // move left animation
-        if(left)
-        {
-            for(int n = lengthOfSnake-1; n>=0;n--)
-            {
+        if(left) {
+            for(int n = lengthOfSnake-1; n>=0;n--) {
                 snakeYLength[n+1] = snakeYLength[n];
             }
-            for(int n = lengthOfSnake; n>=0; n--)
-            {
-                if (n==0)
-                {
+            for(int n = lengthOfSnake; n>=0; n--) {
+                if (n==0) {
                     snakeXLength[n] = snakeXLength[n]-25;
-                }
-                else
-                {
+                } else {
                     snakeXLength[n] = snakeXLength[n-1];
                 }
-                if(snakeXLength[n] < 25)
-                {
+                if(snakeXLength[n] < 25) {
                     snakeXLength[n] = 850;
                 }
-
             }
             repaint();
-
         }
         // move up animation
-        if(up)
-        {
-            for(int n = lengthOfSnake-1; n>=0;n--)
-            {
+        if(up) {
+            for(int n = lengthOfSnake-1; n>=0;n--) {
                 snakeXLength[n+1] = snakeXLength[n];
             }
-            for(int n = lengthOfSnake; n>=0; n--)
-            {
-                if (n==0)
-                {
+            for(int n = lengthOfSnake; n>=0; n--) {
+                if (n==0) {
                     snakeYLength[n] = snakeYLength[n]-25;
-                }
-                else
-                {
+                } else {
                     snakeYLength[n] = snakeYLength[n-1];
                 }
-                if(snakeYLength[n] < 75)
-                {
+                if(snakeYLength[n] < 75) {
                     snakeYLength[n] = 625;
                 }
-
             }
             repaint();
-
         }
         // move down animation
-        if(down)
-        {
-            for(int n = lengthOfSnake-1; n>=0;n--)
-            {
+        if(down) {
+            for(int n = lengthOfSnake-1; n>=0;n--) {
                 snakeXLength[n+1] = snakeXLength[n];
             }
-            for(int n = lengthOfSnake; n>=0; n--)
-            {
-                if (n==0)
-                {
+            for(int n = lengthOfSnake; n>=0; n--) {
+                if (n==0) {
                     snakeYLength[n] = snakeYLength[n]+25;
-                }
-                else
-                {
+                } else {
                     snakeYLength[n] = snakeYLength[n-1];
                 }
-                if(snakeYLength[n] > 625)
-                {
+                if(snakeYLength[n] > 625) {
                     snakeYLength[n] = 75;
                 }
-
             }
-
             repaint();
         }
-
-
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
+    public void keyTyped(KeyEvent e) { }
 
     @Override
     public void keyPressed(KeyEvent e) {
@@ -245,8 +210,8 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
             if(!left){
                 right = true;
             } else {
-              right = false;
-              left = true;
+                right = false;
+                left = true;
             }
             up = false;
             down = false;
@@ -293,11 +258,8 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
             left = false;
             right = false;
         }
-
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
+    public void keyReleased(KeyEvent e) { }
 }
